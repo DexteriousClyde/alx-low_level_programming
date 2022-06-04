5cff7bfd50d264827463bbed4c4b9ec165faedaf
DexteriousClyde <clydebilam@gmail.com>
2022-06-03 18:16:40 -0700

    Task2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,18 @@
 #include <stdio.h>
+
+/**
+ * main - main running code
+ * Return: 0
+ */
+
+int main(void)
+{
+	char letter;
+	
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
