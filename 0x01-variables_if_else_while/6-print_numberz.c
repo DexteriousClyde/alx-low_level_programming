573bbb6cbe43a8328054ea4d6f0aa94117e6d299
DexteriousClyde <clydebilam@gmail.com>
2022-06-03 18:32:20 -0700

    Task6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,18 @@
 #include <stdio.h>
+
+/**
+ * main - main running code
+ * Return: 0
+ */
+
+int main(void)
+{
+       int letter;
+
+	for (letter = 0; letter <= 9; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
