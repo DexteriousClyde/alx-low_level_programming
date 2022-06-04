60aa31198eb67133bcf08cdf04432e99ebf5d433
DexteriousClyde <clydebilam@gmail.com>
2022-06-03 18:26:32 -0700

    Task3

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,22 @@
 #include <stdio.h>
+
+/**
+ * main - main running code
+ * Return: 0
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter == 'q' || letter == 'e')
+		{
+			continue;
+		}
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
