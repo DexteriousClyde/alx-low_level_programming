cbd1848b3e4c07527919d7c35406a58ced6f7db3
DexteriousClyde <clydebilam@gmail.com>
2022-06-03 18:49:51 -0700

    Done

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,22 @@
 #include <stdio.h>
+
+/**
+ * main - main running code
+u * Return: 0
+ */
+
+int main(void)
+{
+	int letter;
+
+	for (letter = 0; letter <= 9; letter++)
+	{
+		putchar(letter + 48);
+	}
+	for (a = 'a'; a < 'g'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,1 +1,23 @@
 #include <stdio.h>
+
+/**
+ * main - main running code
+u * Return: 0
+ */
+
+int main(void)
+{
+	int letter;
+
+	for (letter = 0; letter <= 9; letter++)
+	{
+		putchar(letter + 48);
+		if (letter < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
