1a1499efcabd4c28046d87ff15773f94ebe4c891
DexteriousClyde <clydebilam@gmail.com>
2022-06-03 18:29:06 -0700

    TASK5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,1 +1,18 @@
 #include <stdio.h>
+
+/**
+ * main - main running code
+u * Return: 0
+ */
+
+int main(void)
+{
+	int letter;
+
+	for (letter = 0; letter <= 9; letter++)
+	{
+		printf("%d", letter);
+	}
+	printf('\n');
+	return (0);
+}
