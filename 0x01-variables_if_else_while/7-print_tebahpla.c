1a9263220f9df75487e067983ec588594f7534f7
DexteriousClyde <clydebilam@gmail.com>
2022-06-03 18:37:41 -0700

    Task7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,18 @@
 #include <stdio.h>
+
+/**
+ * main - main running code
+ * Return: 0
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter <= 'a'; letter--)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
