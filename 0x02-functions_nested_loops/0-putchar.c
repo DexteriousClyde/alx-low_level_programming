284416eeff356f20ff8e6a9dbfbe89485943a4c9
DexteriousClyde <clydebilam@gmail.com>
2022-06-07 15:56:50 -0700

    Task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,19 @@
-#include main.h
+#include "main.h"
+
+/**
+ * main - main code
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c[9] = "_putchar";
+	int i = 0;
+
+	while(i < 9)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
