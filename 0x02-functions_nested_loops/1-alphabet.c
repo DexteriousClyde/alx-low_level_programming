252ddd4c7902ecb74bbd4e8ae679cba0fea25905
DexteriousClyde <clydebilam@gmail.com>
2022-06-07 16:12:05 -0700

    Task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,17 @@
-#include main.h
+#include "main.h"
+/**
+ * print_alphabet - main running code
+ * Return: void
+ */
+
+
+
+void print_alphabet(void)
+{
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
+}
