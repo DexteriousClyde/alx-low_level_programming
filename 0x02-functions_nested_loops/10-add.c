ecf92d8cef13c45a2a35468bf17a49901881a25c
DexteriousClyde <clydebilam@gmail.com>
2022-06-07 18:49:10 -0700

    Done

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,1 +1,13 @@
-#include main.h
+#include "main.h"
+
+/**
+ * add - Main code
+ * @a: input
+ * @b: input
+ * Return: t
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,1 +1,23 @@
-#include main.h
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - main code
+ * @n: input
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+	while (n < 98)
+	{
+		printf("%d, ", n);
+		n++;
+	}
+	while (n > 98)
+	{
+		printf("%d, ", n);
+		n--;
+	}
+	printf("98\n");
+}
