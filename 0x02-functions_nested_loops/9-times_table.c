8e17f9b0b42f28e4ded4efaabd5613a9deabe726
DexteriousClyde <clydebilam@gmail.com>
2022-06-07 19:03:47 -0700

    Done

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,1 +1,25 @@
-#include main.h
+#include "main.h"
+
+/**
+ * time_table - main
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int n;
+	int x;
+	int y;
+
+	for (y = 0; y < 10; y++)
+	{
+		for (x = 0; x < 10; x++)
+		{
+			n = x * y;
+			_putchar(n);
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
