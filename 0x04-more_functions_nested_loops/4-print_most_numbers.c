fae6d226c63ba4cb0ee479e36f80147ad357ed41
DexteriousClyde <clydebilam@gmail.com>
2022-06-09 15:33:14 -0700

    task4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,1 +1,22 @@
 #include "main.h"
+
+/**
+ * print_numbers - main code
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i == 2 || i == 4)
+		{
+			continue;
+		}
+		_putchar('0' + i);
+	}
+
+	_putchar('\n');
+}
