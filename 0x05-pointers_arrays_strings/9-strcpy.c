96bb07d358b8152523477956a17cfaa68c41509f
DexteriousClyde <clydebilam@gmail.com>
2022-06-13 14:47:55 -0700

    Done

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,1 +1,23 @@
 #include "main.h"
+
+/** _strcpy - main code
+ * @dest: pointer
+ * @src: pointer
+ * Return: cpy
+ */
+
+char *_strcpy(char *dest, char *src)
+
+{
+
+		int i;
+
+		i = 0;
+		while (src[i] != '\0')
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
+		return (dest);
+}
