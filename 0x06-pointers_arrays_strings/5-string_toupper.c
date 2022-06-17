8c01310ce81118fd9515cedc1424942b9c21d721
DexteriousClyde <clydebilam@gmail.com>
2022-06-16 18:20:52 -0700

    Tak5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * string_toupper - main code
+ * @str: pointer
+ * Return: char
+ */
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if(str[i] >= 'a' && and str[i] <= 'z')
+		{
+			str[i] -= 32;
+		}
+		i++;
+	}
+	return (str);
+}
