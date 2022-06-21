43f3b892ea2d3c03f4563d41458ea4e731bd173e
DexteriousClyde <clydebilam@gmail.com>
2022-06-20 20:10:26 -0700

    Done

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_diagsum - main code to run it all
+ * @a: pointer integer
+ * @size: number
+ * Return: void
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int sum1, sum2, i;
+
+	sum1 = 0;
+	sum2 = 0;
+	for (i = 0; i < size; i++)
+	{
+		sum1 += a[i];
+		sum2 += a[size - i - 1];
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
