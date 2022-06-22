2b1317ac851b4ba45521961c2cf6951ae5ca5dec
DexteriousClyde <clydebilam@gmail.com>
2022-06-22 15:36:39 -0700

    check

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,26 +1,6 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - main recursive code
- * @n: input int
- * Return: sqrt int
- */
-
-int _sqrt_recursion(int n)
-{
-	if (n < 0)
-	{
-		return (-1);
-	}
-	else if (n == 0)
-	{
-		return (0);
-	}
-	else
-		return (check(1, n));
-}
-
-/**
  * check - main
  * @i: int
  * @j: num input
@@ -40,5 +20,26 @@ int check(int i, int j)
 	else
 	{
 		return (-1);
+	}
+}
+/**
+ * _sqrt_recursion - main recursive code
+ * @n: input int
+ * Return: sqrt int
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (check(1, n));
 	}
 }
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,17 +1,6 @@
 #include "main.h"
 
 /**
- * is_prime_number - main
- * @n: int input
- * Return: int
- */
-
-int is_prime_number(int n)
-{
-	return (check_prime(2, n));
-}
-
-/**
  * check_prime - alt
  * @i: int input
  * @j: int
@@ -32,4 +21,15 @@ int check_prime(int i, int j)
 	{
 		return (check_prime(i++, j));
 	}
+}
+
+/**
+ * is_prime_number - main
+ * @n: int input
+ * Return: int
+ */
+
+int is_prime_number(int n)
+{
+	return (check_prime(2, n));
 }
