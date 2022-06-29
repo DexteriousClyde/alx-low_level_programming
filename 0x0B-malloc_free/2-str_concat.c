e98bc036582278d487207d45532c63e7c1a674c8
DexteriousClyde <clydebilam@gmail.com>
2022-06-28 19:02:46 -0700

    try

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,6 +1,25 @@
-#include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
+
+/**
+ * sizes - function
+ * @word: string
+ * Return: num
+ */
+
+int sizes(char *s)
+{
+	int a, j;
+
+	a = 0;
+	for (j = 0; s1[j] != '\0'; j++)
+	{
+		a += 1;
+	}
+	return (a);
+}
+
+
 
 /**
  * str_concat - function
@@ -14,44 +33,23 @@ char *str_concat(char *s1, char *s2)
 	int i, size1, size2;
 	char *ar;
 
-	size1 = 0;
-	size2 = 0;
 	if (s1 == NULL)
-	{
 		size1 = 0;
-	}
 	else
-	{
-		for (i = 0; s1[i] != '\0'; i++)
-		{
-			size1 += 1;
-		}
-	}
+		size1 = sizes(s1);
 	if (s2 == NULL)
-	{
-	}
+		size2 = 0;
 	else
-	{
-		for (i = 0; s2[i] != '\0'; i++)
-		{
-			size2 += 1;
-		}
-	}
+		size2 = sizes(s2);
 	ar = malloc((size1 * sizeof(*s1)) + (size2 * sizeof(*s2)) + 1);
 	if (ar == NULL)
-	{
 		return (NULL);
-	}
 	for (i = 0; i < size1 + size2; i++)
 	{
 		if (i < size1)
-		{
 			ar[i] = s1[i];
-		}
 		else
-		{
 			ar[i] = s2[i - size1];
-		}
 	}
 	ar[i] = '\0';
 	return (ar);
