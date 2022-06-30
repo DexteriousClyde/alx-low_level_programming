b119695b77efe1a4b7d7e44bf2cb82766352818b
DexteriousClyde <clydebilam@gmail.com>
2022-06-30 13:44:29 -0700

    task1

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * *malloc_checked - function
+ * @b: int
+ * Return: mem address
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *ar = malloc(b);
+	if (ar == NULL)
+	{
+		exit(98);
+	}
+	return (ar);
+}
---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,59 @@
+#include "main.ih"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * sizes - func
+ * @s: pointer
+ * Return: num
+ */
+
+int sizes(char *s)
+{
+	int len;
+
+	for (len = 0; s[len] != '\0'; len++)
+		;
+	return (len);
+}
+
+/**
+ * *string_nconcat - function
+ * @s1: pointer
+ * @s2: pointer
+ * @n: int
+ * Return: char
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *arr;
+	int i, j1, k2;
+
+	i = 0;
+	if (s1 == NULL)
+		j1 = 0;
+	else
+		j1 = sizes(s1);
+	if (s2 == NULL)
+		k2 = 0;
+	else
+		k2 = sizes(s2);
+	if (k2 > n)
+		arr = (j1 + 1 + (n * sizeof(char)));
+	else
+		arr = malloc(k2 + j1 + 1);
+
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i < j1 + k2; i++)
+		if (i < j1)
+			arr[i] = s1[i];
+		else
+		{
+			if(n < k2 && i - j1 => n)
+				break;
+			arr[i] = s2[i-j1];
+		}
+	return (arr);
+}
