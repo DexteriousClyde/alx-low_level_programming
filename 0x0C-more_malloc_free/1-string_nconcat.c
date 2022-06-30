9bae4487d8f228912beb7e93d71e0a4a9b8752df
DexteriousClyde <clydebilam@gmail.com>
2022-06-30 14:21:21 -0700

    task1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,6 +3,20 @@
 #include <stdio.h>
 
 /**
+ * sizes - func
+ * @s: pointer
+ * Return: num
+ */
+
+int sizes(char *s)
+{
+	int len;
+	for (len = 0; s[len] != '\0'; len++)
+		;
+	return (len);
+}
+
+/**
  * *string_nconcat - function
  * @s1: pointer
  * @s2: pointer
@@ -13,28 +27,32 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *arr;
-	int i;
-
+	int i, j1, k2;
+	
 	i = 0;
-	j = 0;
-	arr = malloc();
+	if (s1 == NULL)
+		j1 = 0;
+	else
+		j1 = sizes(s1);
+	if (s2 == NULL)
+		k2 = 0;
+	else
+		k2 = sizes(s2);
+	if (k2 > n)
+		arr = (j1 + 1 + (n * sizeof(char)));
+	else
+		arr = malloc(k2 + j1 + 1);
 	if (arr == NULL)
 		return (NULL);
-	while (s1[i] != '\0' s2 != '\0')
-	{
-		if (s1[i] != '\0')
-		{
+	for (i = 0; i < j1 + k2; i++)
+		if (i < j1)
 			arr[i] = s1[i];
-		}
 		else
 		{
-			if (s2[j] != '\0')
-			{
-				arr[i] = s2[j];
-				j++;
-			}
+			if(n < k2 && i - j1 => n)
+				break;
+			arr[i] = s2[i-j1];
 		}
-		i++;
 	}
 	return (arr);
 }
