bce5eee312b74340d424a710a8ba845739c40cf6
DexteriousClyde <clydebilam@gmail.com>
2022-07-14 21:30:11 -0700

    D101

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
+/**
+ * first - main
+ */
 
-void (void) __attribute__ ((constructor)) first()
+void __attribute__ ((constructor)) first()
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
