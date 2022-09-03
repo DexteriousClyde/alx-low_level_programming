b482e83d5f1bcaebd3401f01c8c2e0deb1f38a38
DexteriousClyde <clydebilam@gmail.com>
2022-09-02 20:02:03 -0700

    hi tak ku yini

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,12 +9,13 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	const dlistint_t *temp;
-	size_t i = 0;
+	size_t i;
 	
+	i = 0;
 	temp = h;
 	while (temp != NULL)
 	{
-		printf("%i\n", temp->n);
+		printf("%d\n", temp->n);
 		temp = temp->next;
 		i++;
 	}
