423a4e450c881d7ae190c8923f3bd86b2928b63f
DexteriousClyde <clydebilam@gmail.com>
2022-09-02 19:55:49 -0700

    Hayii

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - main
+ * @h: pointer
+ * Return: void
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *temp;
+	size_t i = 0;
+	
+	temp = h;
+	while (temp != NULL)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+		i++;
+	}
+	return (i);
+}
